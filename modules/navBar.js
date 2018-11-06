cbb6c8c2db578367198db293d1ed436ffe0e370b
jose <josevonchongtrabajo@outlook.com>
2018-11-06 05:35:34 -0600

    index modification

---
+++ modules/navBar.js
@@ -0,0 +1,8 @@
+let navBar = document.getElementById("nav");
+navBar.insertAdjacentHTML("afterbegin", '<nav>\n' +
+    '    <div class="nav-wrapper white">\n' +
+    '      <a href="#" class="brand-logo black-text" style="padding-left: 15px">Resume</a>\n' +
+    '      <ul id="nav-mobile" class="right hide-on-med-and-down">\n' +
+    '      </ul>\n' +
+    '    </div>\n' +
+    '  </nav>');
